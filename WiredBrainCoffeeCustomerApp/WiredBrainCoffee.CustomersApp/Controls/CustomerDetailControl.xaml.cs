9a86afd3cedc43cff622a7930d455a135a4d160c
varshaauti27 <56707382+varshaauti27@users.noreply.github.com>
2020-08-14 15:38:59 -0500

    WiredBrainCoffee Updated

---
+++ WiredBrainCoffeeCustomerApp/WiredBrainCoffee.CustomersApp/Controls/CustomerDetailControl.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Markup;
+using WiredBrainCoffee.CustomersApp.Model;
+
+namespace WiredBrainCoffee.CustomersApp.Controls
+{
+    [ContentProperty(Name = nameof(Customer))]
+    public sealed partial class CustomerDetailControl : UserControl
+    {
+        public static readonly DependencyProperty CustomerProperty =
+            DependencyProperty.Register("Customer", typeof(Customer),
+              typeof(CustomerDetailControl), new PropertyMetadata(null));
+
+        public CustomerDetailControl()
+        {
+            this.InitializeComponent();
+        }
+
+        public Customer Customer
+        {
+            get { return (Customer)GetValue(CustomerProperty); }
+            set { SetValue(CustomerProperty, value); }
+        }
+
+        private async void ButtonEdit_Click(object sender, RoutedEventArgs e)
+        {
+            MessageDialog messageDialog = new MessageDialog("Edit");
+            await messageDialog.ShowAsync();
+        }
+    }
+}
