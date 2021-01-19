174e803e56cac18bb2769de045ff5c7ae611cfd1
varshaauti27 <56707382+varshaauti27@users.noreply.github.com>
2021-01-19 12:22:56 -0600

    Update

---
+++ ControlTemplates/ControlTemplates/CustomControl1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace ControlTemplates
+{
+    public class CustomControl1 : Control
+    {
+        static CustomControl1()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(CustomControl1), 
+                new FrameworkPropertyMetadata(typeof(CustomControl1)));
+        }
+
+        Shape _backdrop = null;
+
+        public CustomControl1()
+        {
+            MouseLeftButtonDown += ChangeColor;
+        }
+
+        public override void OnApplyTemplate()
+        {
+            _backdrop = GetTemplateChild("PART_Backdrop") as Shape;
+        }
+
+        private void ChangeColor(object sender, MouseButtonEventArgs e)
+        {
+            if (_backdrop != null)
+            {
+                _backdrop.Fill = new SolidColorBrush(Colors.Blue);
+            }
+        }
+    }
+}
